384b6a571b488fd156d3278003745f74f558ab3f
Smitesh Das <57407736+smiteshdas@users.noreply.github.com>
2021-07-20 15:48:11 +0530

    Add files via upload

---
+++ tone.js
@@ -0,0 +1,32 @@
+/*
+let x=5;
+let y = 5;
+let a = 0.01;
+let vx=0;
+let vy = 0;*/
+
+let x;
+
+function setup(){
+				createCanvas(600,600);
+				background(245);
+				x = new p5.AudioIn();
+				x.start()
+				
+				
+}
+function draw(){
+				text(x.getLevel(),300,300)
+
+				
+}
+				
+		  	
+				
+  			  			
+				
+			
+
+				
+				
+
